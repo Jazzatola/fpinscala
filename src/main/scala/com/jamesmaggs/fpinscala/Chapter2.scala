77276afe88f551716dca083cddfefff1d5b50c3d
James Maggs <james.maggs@springer.com>
2017-06-28 13:14:22 +0100

    Exercise 2.4 - uncurry.

--- src/main/scala/com/jamesmaggs/fpinscala/Chapter2.scala
+++ src/main/scala/com/jamesmaggs/fpinscala/Chapter2.scala
@@ -30,4 +30,7 @@ object Chapter2 {
   def curry[A,B,C](f: (A, B) => C): A => (B => C) =
     (a: A) => (b: B) => f(a, b)
 
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C =
+    (a: A, b: B) => f(a)(b)
+
 }
