7ccb9199a8be45b3a6f5ed2afbf91cf5a8a50792
James Maggs <james.maggs@springer.com>
2017-07-05 11:22:06 +0100

    Exercise 3.21 - filter in terms of flatMap.

--- src/main/scala/com/jamesmaggs/fpinscala/Chapter3.scala
+++ src/main/scala/com/jamesmaggs/fpinscala/Chapter3.scala
@@ -59,7 +59,7 @@ object List {
 
   def map[A,B](as: List[A])(f: A => B): List[B] = foldLeft(as, List[B]())((acc, a) => Cons(f(a), acc))
 
-  def filter[A](as: List[A])(f: A => Boolean): List[A] = foldRight(as, List[A]())((a, acc) => if(f(a)) Cons(a, acc) else acc)
+  def filter[A](as: List[A])(f: A => Boolean): List[A] = flatMap(as)(a => if(f(a)) List(a) else Nil)
 
   def flatMap[A,B](as: List[A])(f: A => List[B]): List[B] = foldRight(as, List[B]())((a, acc) => append(f(a), acc))
 }
